7683e2e14621ec57fac2e3d168d94c6121dd141c
sealucky7 <sea.yastreb@gmail.com>
2015-09-22 12:27:54 +0300

    sorry)

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ function news ($id, $news){
 // если параметр не был передан - выводить 404 ошибку
 // http://php.net/manual/ru/function.header.php
 if($_GET){
-    if(array_key_exists('id', $_GET) && is_numeric($_POST['id'])){
+    if(array_key_exists('id', $_GET) && is_numeric($_GET['id'])){
     news($_GET['id'], $news);
     }
  else {
